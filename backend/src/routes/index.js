bf6b13468c434121839365490d82a5f97d95c581
unknown <apriadi1163@gmail.com>
2022-08-07 19:56:29 +0700

    update-making-tables

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const { addUSer, getUsers, getUser, updateUser, deleteUser} = require("../controllers/user")
 const { register, login } = require("../controllers/auth")
+const { getWallet, updateWallet } = require("../controllers/wallet")
 const { auth } = require("../middleware/auth")
 
 router.post("/user", addUSer);
@@ -11,6 +12,9 @@ router.get("/user", getUsers);
 router.get("/user/:id", getUser)
 router.patch("/user/:id", updateUser)
 router.delete("/user/:id", deleteUser)
+
+router.get("/wallet", auth, getWallet)
+router.patch("/wallet", auth, updateWallet)
 
 router.post("/register", register)
 router.post("/login", login)
