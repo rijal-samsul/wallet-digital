391f60b563ff354598eb496f04411a305ff8bef8
Your Name <samsul081@mail.com>
2022-08-07 16:43:01 +0700

    resolve conflict

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route, useNavigate } from "react-router-dom"
 import LandingPage from "./pages/landingPage";
 import About from "./pages/About";
+import Profile from "./pages/profile";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage/>} />
       <Route path="/about" element={<About />} />
+      <Route path="/profile" element={<Profile />} />
     </Routes>
   );
 }
